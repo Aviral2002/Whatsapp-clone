9b1c0cc4eea5262291544b99253d98fe844faaad
Aviral Jain <jainaviral2002@gmail.com>
2022-07-10 17:01:40 +0530

    gg

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCdNGkelLaD9XWMbrled77qZUMAKDNLCqg",
+    authDomain: "clone-f4949.firebaseapp.com",
+    projectId: "clone-f4949",
+    storageBucket: "clone-f4949.appspot.com",
+    messagingSenderId: "504506314175",
+    appId: "1:504506314175:web:9c45c46398251d6633850a"
+  };
